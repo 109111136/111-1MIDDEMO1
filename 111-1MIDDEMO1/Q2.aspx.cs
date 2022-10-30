ee99204bddcdf80b45c592ae4bae4109f240e839
109111136 <h226810797@gmail.com>
2022-10-31 01:12:39 +0800

    finish

--- 111-1MIDDEMO1/Q2.aspx.cs
+++ 111-1MIDDEMO1/Q2.aspx.cs
@@ -14,7 +14,41 @@ namespace _111_1MIDDEMO1 {
         };
 
         protected void Page_Load(object sender, EventArgs e) {
-
+            if (!IsPostBack){
+                for (int city=0; city<s_City.Length; city++){
+                    ListItem C = new ListItem();
+                    C.Text = C.Value = s_City[city];
+                    dpl_City.Items.Add(C);
+                }
+                /*ct_List();*/
+            }
+            if (dpl_City.SelectedIndex != 1)
+            {
+                dpl_Area.Items.Clear();
+                dpl_Area.Items.Add("中正區");
+                dpl_Area.Items.Add("文山區");
+                dpl_Area.Items.Add("大安區");
+            }
+            else
+            {
+                dpl_Area.Items.Clear();
+                dpl_Area.Items.Add("淡水區");
+                dpl_Area.Items.Add("石碇區");
+                dpl_Area.Items.Add("土城區");
+            }
         }
+        protected void dpl_City_SelectedIndexChanged(object sender, EventArgs e){
+            /*ct_List();*/
+        }
+        /*protected void ct_List() {
+            int a = dpl_City.SelectedIndex;
+            dpl_Area.Items.Clear();
+            for (int city = 0; city < s_Area.GetLength(1); city++)
+            {
+                ListItem C = new ListItem();
+                C.Text = C.Value = s_Area[a, city];
+                dpl_Area.Items.Add(C);
+            }
+        }*/
     }
 }
